568db4990e57352151339b348d250f05e897bcb4
malum-labs <75781092+malum-labs@users.noreply.github.com>
2020-12-10 18:00:00 +0300

    Максимальное из 4 чисел

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0419/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task04.task0419;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Максимум четырех чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        System.out.println(Math.max(a, Math.max(b, Math.max(c, d))));
+
+    }
+}
